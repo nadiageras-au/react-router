49d2fd07de7870a33898a4a6e3f613296e024d4b
nadia <nadiageras.au@gmail.com>
2024-01-28 00:14:32 +1000

    refactoring - styled components...

--- src/App.tsx
+++ src/App.tsx
@@ -5,45 +5,45 @@ import {PageOne} from "./components/pages/PageOne";
 import {PageTwo} from "./components/pages/PageTwo";
 import {PageThree} from "./components/pages/PageThree";
 import {Error404} from "./components/pages/Error404";
-import styled from "styled-components";
+import {S} from "./components/pages/_styles";
 
+const PATH = {
+    PAGE1:'/page1',
+    PAGE2:'/page2',
+    PAGE3:'/page3',
+    ERROR404: '/error404',
 
+}
 function App() {
     return (
         <div>
             <div className={styles.header}><h1>HEADER</h1></div>
             <div className={styles.body}>
                 <nav className={styles.nav}>
-                    <div>
-                        <NavLink
-                            to={'/page1'}
-                            className={({isActive})=>isActive ? styles.active : styles.navLink}>
+                    <S.NavWrapper>
+                        <NavLink to={PATH.PAGE1}>
                             Page 1
                         </NavLink>
-                    </div>
-                    <div>
-                        <NavLink
-                            to={'/page2'}
-                            className={({isActive})=>isActive ? styles.active : styles.navLink}>
-                        Page 2
-                    </NavLink>
-                    </div>
-                    <div>
-                        <NavLink to={'/page3'}
-                                 className={({isActive})=>isActive ? styles.active : styles.navLink}>
-                        Page 3
-                    </NavLink>
-                    </div>
+                    </S.NavWrapper>
+                    <S.NavWrapper>
+                        <NavLink to={PATH.PAGE2}>
+                            Page 2
+                        </NavLink>
+                    </S.NavWrapper>
+                    <S.NavWrapper>
+                        <NavLink to={PATH.PAGE3}>
+                            Page 3
+                        </NavLink>
+                    </S.NavWrapper>
 
                 </nav>
                 <div className={styles.content}>
                     <Routes>
                         <Route path="/" element={<Navigate to={"/page1"}/>}/>
-                        <Route path="/page1" element={<PageOne/>}/>
-                        <Route path="/page2" element={<PageTwo/>}/>
-                        <Route path="/page3" element={<PageThree/>}/>
-                        <Route path="/error404" element={<Error404/>}/>
-                        <Route path="/*" element={<Navigate to={"/error404"}/>}/>
+                        <Route path={PATH.PAGE1} element={<PageOne/>}/>
+                        <Route path={PATH.PAGE2} element={<PageTwo/>}/>
+                        <Route path={PATH.PAGE3} element={<PageThree/>}/>
+                        <Route path="/*" element={PATH.ERROR404}/>
                     </Routes>
 
                 </div>
@@ -56,21 +56,4 @@ function App() {
 
 export default App;
 
-const NavWrapper = styled.div`
-margin-left: 10px;
-  font-size: 20px;
-  
-  & > a {
-    text-decoration: none;
-    color: #1e3786;
-  }
-  
-  & > a.active {
-    text-decoration: none;
-    color: #03eaff;
-  }
-  
-  & > a:hover {
-    color: steelblue;
-  }
-`
+
